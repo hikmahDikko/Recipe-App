8b0635b4fda23659167ecc3484fa834adfb18eb4
Hikmah Dikko <dhywis31@gmail.com>
2023-12-25 15:36:45 +0100

    new

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,18 +8,23 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipes.component.css'],
   providers: [RecipeService]
 })
-export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
+export class RecipesComponent  {
+  // selectedRecipe: Recipe;
+  // private recipeSub : Subscription
 
-  constructor(private recipeService: RecipeService) { }
+  // constructor(private recipeService: RecipeService) { }
 
-  ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
-  }
+  // ngOnInit() {
+  //   this.recipeSub = this.recipeService.recipeSelected
+  //     .subscribe(
+  //       (recipe: Recipe) => {
+  //         this.selectedRecipe = recipe;
+  //       }
+  //     );
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.recipeSub.unsubscribe();
+  // }
 
 }
