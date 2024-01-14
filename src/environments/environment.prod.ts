1967566dedd5fe276c4d67cc62d784336d325b67
Hikmah Dikko <dhywis31@gmail.com>
2024-01-14 18:26:18 +0100

    deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    firebaseAPIKey : 'AIzaSyC1zhWRhuZ_7PYVaSV6ia34BFom6hPLGec'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    firebaseAPIKey : 'AIzaSyC1zhWRhuZ_7PYVaSV6ia34BFom6hPLGec'
+}
